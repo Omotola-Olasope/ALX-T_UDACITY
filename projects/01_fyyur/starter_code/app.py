31a3526f9cf01a1a5b7449b67117a0ffd469698b
Omotola Olasope <tola.olasope@gmail.com>
2022-05-24 13:08:38 +0100

    further changes to Shows app routes

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -645,11 +645,30 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
+  
+  try:
+    show_artistId = request.form['artist_id']
+    show_venueId = request.form['venue_id']
+    show_startTime = request.form['start_time']
+    
+    new_show = Show(show_artisId, show_venueId, show_startTime)
+    
+    db.session.add(new_show)
+    db.session.commit()
+  
+    flash('Show was successfully listed!')
+  
+  except:
+    db.session.rollback()
+    flash('An error occurred. Show could not be listed.')
+  
+  finally:
+    db.session.close()
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
 
   # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  #flash('Show was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
